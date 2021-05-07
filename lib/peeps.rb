e2d288e5a0cf739984df1e1420e6896c092cc080
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-05-07 12:20:31 +0200

    databaseConnection now called in Peeps

--- lib/peeps.rb
+++ lib/peeps.rb
@@ -1,4 +1,5 @@
 require 'pg'
+require 'database_connection'
 
 class Peeps
   attr_reader :id, :name, :username, :peep, :time
@@ -12,46 +13,24 @@ class Peeps
   end
 
   def self.all
-    if ENV['Environment'] = 'test'
-      connection = PG.connect(dbname: 'new_chitter_test')
-    else
-      connection = PG.connect(dbname: 'chitter')
-    end
-    result = connection.exec("SELECT * FROM peeps;")
+    result = DatabaseConnection.query("SELECT * FROM peeps;")
     result.map do |peep| 
       Peeps.new(id: peep['id'], name: peep['name'], username: peep['username'], peep: peep['peep'], time: peep['time']) 
     end
   end
 
   def self.create(name:, username:, peep:)
-    if ENV['Environment'] = 'test'
-      connection = PG.connect(dbname: 'new_chitter_test')
-        else
-          connection = PG.connect(dbname: 'chitter')
-        end
-    
-    result = connection.exec("INSERT INTO peeps (name, username, peep) VALUES('#{name}', '#{username}', '#{peep}') RETURNING id, name, username, peep, time")
+    result = DatabaseConnection.query("INSERT INTO peeps (name, username, peep) VALUES('#{name}', '#{username}', '#{peep}') RETURNING id, name, username, peep, time")
     Peeps.new(id: result[0]['id'], name: result[0]['name'], username: result[0]['username'], peep: result[0]['peep'], time: result[0]['time'])
     end
 
   def self.delete(id:)
-    if ENV['Environment'] = 'test'
-      connection = PG.connect(dbname: 'new_chitter_test')
-        else
-          connection = PG.connect(dbname: 'chitter')
-        end
-    connection.exec("DELETE FROM peeps WHERE id = #{id}")
+    DatabaseConnection.query("DELETE FROM peeps WHERE id = #{id}")
     end
 
   def self.update(id:, name:, username:, peep:)
-    if ENV['Environment'] = 'test'
-      connection = PG.connect(dbname: 'new_chitter_test')
-        else
-          connection = PG.connect(dbname: 'chitter')
-        end
-    result = connection.exec("UPDATE peeps SET peep = '#{peep}' WHERE id = #{id} RETURNING id, name, username, peep, time;")
+    result = DatabaseConnection.query("UPDATE peeps SET peep = '#{peep}' WHERE id = #{id} RETURNING id, name, username, peep, time;")
     Peeps.new(id: result[0]['id'], name: result[0]['name'], username: result[0]['username'], peep: result[0]['peep'], time: result[0]['time'])
-
-      end
+  end
 
 end
