be05ffb2d0b8b21bfbedd07ef3cca19cb26b7e17
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-05-07 11:54:41 +0200

    passing test for using DatabaseConnection class to query database

--- spec/unit/database_connection_spec.rb
+++ spec/unit/database_connection_spec.rb
@@ -15,4 +15,14 @@ describe DatabaseConnection do
             expect(DatabaseConnection.connection).to eq connection
         end
     end
+
+    describe '.query' do
+        it 'executes a query via PG' do
+            connection = DatabaseConnection.setup('new_chitter_test')
+
+            expect(connection).to receive(:exec).with("SELECT * FROM peeps;")
+
+            DatabaseConnection.query("SELECT * FROM peeps;")
+        end
+    end
 end
