bbfb5642735a40b6b7e923008c487e149f4cacb2
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-05-12 13:46:31 +0200

    updated test for index page, more thorough

--- spec/features/viewing_index_spec.rb
+++ spec/features/viewing_index_spec.rb
@@ -2,5 +2,8 @@ feature 'Viewing Index' do
   scenario 'visiting the index page' do
     visit('/')
     expect(page).to have_content "Chitter"
+    expect(page).to have_link('Sign Up')
+    expect(page).to have_link('Sign In')
+    expect(page).not_to have_button('Sign Out')
   end
 end
