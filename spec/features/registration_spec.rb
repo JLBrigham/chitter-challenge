0322c01117f860bfeb90f7f3f0fed45e132051d0
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-05-12 17:25:10 +0200

    create layout and app.css files

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -1,6 +1,6 @@
 feature 'registration' do
     scenario 'a user can sign up' do
-     sign_up
+    sign_up
     expect(page).to have_content "Chitter"
     expect(page).not_to have_button 'Sign Out'
     end
